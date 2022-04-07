8662cfa7d48925334af0d88bb53c24efdcf9fe67
Natalia <natyargumedo@hotmail.com>
2022-04-07 00:00:32 -0700

    added index show page for places

---
+++ views/places/index.jsx
@@ -0,0 +1,17 @@
+const React = require('react')
+const Def = require('./default')
+
+function index () {
+    return (
+        <html>
+            <head>
+                <title>INDEX</title>
+            </head>
+            <body>
+              
+            </body>
+        </html>
+    )
+  }
+
+module.exports = index
