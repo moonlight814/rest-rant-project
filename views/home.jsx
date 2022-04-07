68f66a85e4c60c691dad4a9cfee3aafd6bec940c
Natalia <natyargumedo@hotmail.com>
2022-04-07 01:43:29 -0700

    added pictures to 404 page and home page

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                  <img src="/images/image_homepage.jpg" alt="variety of foods on plate" />
+              <div id= 'anchorImage'>
+                  Photo by <a href="https://unsplash.com/@lvnatikk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brenda Godiner</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              </div>
+              </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
