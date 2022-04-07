6018fd2403fcd59fb5054c9ebe1d8c8ebc3f9d37
Natalia <natyargumedo@hotmail.com>
2022-04-07 01:26:41 -0700

    adding styles to defaultjsx

--- views/home.jsx
+++ views/home.jsx
@@ -6,8 +6,12 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
           </main>
       </Def>
+      
     )
   }
 
