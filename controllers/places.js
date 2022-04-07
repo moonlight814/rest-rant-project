ab2c6732be7e17f7d1b6d6294f3c418960bae955
Natalia <natyargumedo@hotmail.com>
2022-04-07 01:53:41 -0700

    adding bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -6,14 +6,14 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/h-thai-ml-tables.jpg'
       }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]                                                           
+          name: 'Coding Cat Cafe',
+          city: 'Phoenix',
+          state: 'AZ',
+          cuisines: 'Coffee, Bakery',
+          pic: '/images/coffee-cat.jpg'
+      }]                                                        
     res.render('places/index', {places})
     // console.log('index rendered')
 })
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -3,18 +3,26 @@ const Def = require('../default')
 
 function index (data) {
     let placesFormatted = data.places.map((place) => {
-      return (
-        <div>
-          <h2>{place.name}</h2>
-          <img src={place.pic} alt={place.name}/>
-        </div>
-      )
-    })
+        return (
+          <div className="col-sm-6">
+            <h2>{place.name}</h2>
+            <p>
+              {place.cuisines}
+            </p>
+            <img src={place.pic} alt={place.name} />
+            <p>
+              Located in {place.city}, {place.state}
+            </p>
+          </div>
+        )
+      })
     return (
       <Def>
           <main>
               <h1>PLACES INDEX PAGE</h1>
+              <div className="row">
               {placesFormatted}
+              </div>
           </main>
       </Def>
   )
