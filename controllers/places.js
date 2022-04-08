bf2a6ab132574e1d24a025b38c52a38942ea0cb3
Natalia <natyargumedo@hotmail.com>
2022-04-07 23:43:02 -0700

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -12,7 +12,20 @@ router.get('/new', (req, res) => {
   })
   
   
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', {place: places[id]})
+    }
+})
   
+
 router.post('/', (req, res) => {
     // console.log(req.body)
     if (!req.body.pic) {
