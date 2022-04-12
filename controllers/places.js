e1bcab43fe7beb4e1f1867d0ad3a7ef9264051aa
Natalia <natyargumedo@hotmail.com>
2022-04-12 12:27:19 -0700

    show page loads when clicked from index page

--- controllers/places.js
+++ controllers/places.js
@@ -23,8 +23,15 @@ router.post('/', (req, res) => {
   })
 })
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
 router.get('/:id', (req, res) => {
